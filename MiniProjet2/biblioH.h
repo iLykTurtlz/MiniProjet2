1c866b0e7b9ae7ccb55c3b189d4ff9f3134b774a
Margaux971 <99265418+Margaux971@users.noreply.github.com>
2022-02-21 11:04:38 +0100

    réctification et ajout

--- MiniProjet2/biblioH.h
+++ MiniProjet2/biblioH.h
@@ -26,12 +26,12 @@ typedef struct table {
 
 int fonctionClef(char *auteur);
 LivreH *creer_livre(int num, char *titre, char *auteur);
-LivreH *creer_livre(int num, char *titre, char *auteur);
 BiblioH *creer_biblio(int m);
 void liberer_liste(LivreH *l);
 void liberer_biblio(BiblioH *b);
 int fonctionHachage(int cle, int m);
 void inserer_en_tete(LivreH *liste, LivreH *livre);
+void inserer_copie_en_tete(LivreH *liste, int num, char *titre, char *auteur)
 void inserer(BiblioH *b, int num, char *titre, char *auteur);
 
 void afficher_livre(LivreH *l);
@@ -39,9 +39,10 @@ void afficher_biblio(BiblioH *b);
 LivreH *rechercher_livre_num(BiblioH *b, int num);
 LivreH *rechercher_livre_titre(BiblioH *b, char *titre);
 LivreH *rechercher_livre_auteur(BiblioH *b, char *auteur);
+LivreH *recherche_liste_titre_auteur(LivreH *liste, char *titre, char *auteur)
 void supprimer_livre(BiblioH *b, int num, char *titre, char *auteur);
 void fusionner_biblio(BiblioH *b1, BiblioH *b2);
-
+ LivreH *plusieurs_exemp(BiblioH *b);
 
 
 #endif
